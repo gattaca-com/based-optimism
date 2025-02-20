25b945669ba8c890411e0e2572969d65995a7a65
Tomás Arjovsky <t.arjovsky@gmail.com>
2025-02-20 19:30:29 +0100

    Preconf event ordering in op node (#124)
    Co-authored-by: ilitteri <ilitteri@fi.uba.ar>

--- op-node/node/node.go
+++ op-node/node/node.go
@@ -24,6 +24,7 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/conductor"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/driver"
+	"github.com/ethereum-optimism/optimism/op-node/rollup/engine"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/event"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/interop"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/interop/managed"
@@ -69,6 +70,8 @@ type OpNode struct {
 	p2pSigner p2p.Signer            // p2p gossip application messages will be signed with this signer
 	tracer    Tracer                // tracer to get events for testing/debugging
 	runCfg    *RuntimeConfig        // runtime configurables
+
+	preconfChannels engine.PreconfChannels
 
 	safeDB closableSafeDB
 
@@ -161,6 +164,7 @@ func (n *OpNode) init(ctx context.Context, cfg *Config) error {
 	if err := n.initMetricsServer(cfg); err != nil {
 		return fmt.Errorf("failed to init the metrics server: %w", err)
 	}
+	n.preconfChannels = engine.StartPreconf(ctx, n.l2Source)
 	n.metrics.RecordInfo(n.appVersion)
 	n.metrics.RecordUp()
 	if err := n.initPProf(cfg); err != nil {
@@ -640,6 +644,7 @@ func (n *OpNode) PublishNewFrag(ctx context.Context, from peer.ID, frag *eth.Sig
 }
 
 func (n *OpNode) PublishSealFrag(ctx context.Context, from peer.ID, seal *eth.SignedSeal) error {
+
 	n.tracer.OnPublishSealFrag(ctx, from, seal)
 
 	// publish to p2p, if we are running p2p at all
@@ -655,6 +660,7 @@ func (n *OpNode) PublishSealFrag(ctx context.Context, from peer.ID, seal *eth.Si
 }
 
 func (n *OpNode) PublishEnv(ctx context.Context, from peer.ID, env *eth.SignedEnv) error {
+
 	n.tracer.OnPublishEnv(ctx, from, env)
 
 	// publish to p2p, if we are running p2p at all
@@ -699,17 +705,8 @@ func (n *OpNode) OnNewFrag(ctx context.Context, from peer.ID, frag *eth.SignedNe
 	// }
 
 	n.tracer.OnNewFrag(ctx, from, frag)
-
 	n.log.Info("Received new fragment", "frag", frag)
-
-	// Pass on the event to the L2 Engine
-	ctx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
-	if err := n.l2Driver.OnNewFrag(ctx, frag); err != nil {
-		n.log.Warn("failed to notify engine driver of new fragment", frag, "err", err)
-	}
-
+	n.preconfChannels.SendFrag(frag)
 	return nil
 }
 
@@ -721,17 +718,8 @@ func (n *OpNode) OnSealFrag(ctx context.Context, from peer.ID, seal *eth.SignedS
 	// }
 
 	n.tracer.OnSealFrag(ctx, from, seal)
-
 	n.log.Info("Received new seal", "seal", seal)
-
-	// Pass on the event to the L2 Engine
-	ctx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
-	if err := n.l2Driver.OnSealFrag(ctx, seal); err != nil {
-		n.log.Warn("failed to notify engine driver of new seal", seal, "err", err)
-	}
-
+	n.preconfChannels.SendSeal(seal)
 	return nil
 }
 
@@ -743,17 +731,8 @@ func (n *OpNode) OnEnv(ctx context.Context, from peer.ID, env *eth.SignedEnv) er
 	// }
 
 	n.tracer.OnEnv(ctx, from, env)
-
 	n.log.Info("Received new env", "env", env)
-
-	// Pass on the event to the L2 Engine
-	ctx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
-	if err := n.l2Driver.OnEnv(ctx, env); err != nil {
-		n.log.Warn("failed to notify engine driver of new env", env, "err", err)
-	}
-
+	n.preconfChannels.SendEnv(env)
 	return nil
 }
 
