f96d5b7c91cd13b573738d957c65e47c76b4befc
Hamdi Allam <hamdi.allam97@gmail.com>
2025-03-02 14:49:29 -0700

    test contract

---
+++ packages/contracts-bedrock/test/libraries/Promise.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { Test } from "forge-std/Test.sol";
+import { IL2ToL2CrossDomainMessenger } from "interfaces/L2/IL2ToL2CrossDomainMessenger.sol";
+import { Promise } from "src/libraries/Promise.sol";
+import { Predeploys } from "src/libraries/Predeploys.sol";
+
+contract PromiseTest is Test { }
