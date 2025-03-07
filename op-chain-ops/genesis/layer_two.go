b604cb21c28257da09ff145608f635e87c099c1f
alan <652732310@qq.com>
2025-03-07 20:25:02 +0800

    mod LatestHF

--- op-chain-ops/genesis/layer_two.go
+++ op-chain-ops/genesis/layer_two.go
@@ -28,8 +28,8 @@ const (
 	L2AllocsGranite  L2AllocsMode = "granite"
 	L2AllocsHolocene L2AllocsMode = "holocene"
 	L2AllocsIsthmus  L2AllocsMode = "isthmus"
-	// This needs to be updated whenever the latest hardfork is changed.
-	LatestHF L2AllocsMode = L2AllocsIsthmus
+	// This needs to be updated whenever the latest code-complete hardfork is changed.
+	LatestHF L2AllocsMode = L2AllocsHolocene
 )
 
 var (
