3ac6e5d8658b27fe677c1072fc4b0d4b5b8fe9e7
Disco <131301107+0xDiscotech@users.noreply.github.com>
2025-03-14 13:24:34 -0300

    feat: access list (#326)
    * feat: access list poc
    
    * chore: remove is deposit tests
    
    * fix: natspec and enhance comments
    
    * chore: remove l1 block interop on solidity files
    
    * fix: pr fixes
    
    * chore: remove go logic over l1 block interop
    
    * chore: remove some l1 block interop stuff on solidity side
    
    * chore: remove from frozen files
    
    * chore: remove missing l1 block interop stuff on go tests
    
    * refactor: improve natspec and comments
    
    * feat: add is warm tests
    
    * feat: wip trying to integrate with common test
    
    * fix: integrate with common test reading artifact json
    
    * fix: pre-pr and tests
    
    * fix: tests
    
    * fix: tests
    
    Co-authored-by: 0xng <ng@defi.sucks>
    Co-authored-by: skeletor-spaceman <skeletor@defi.sucks>
    
    * fix: pr review comments
    
    * fix: op e2e inbox interface
    
    ---------
    
    Co-authored-by: agusduha <agusnduha@gmail.com>
    Co-authored-by: 0xng <ng@defi.sucks>
    Co-authored-by: skeletor-spaceman <skeletor@defi.sucks>

--- op-e2e/e2eutils/interop/contracts/src/ICrossL2Inbox.sol
+++ op-e2e/e2eutils/interop/contracts/src/ICrossL2Inbox.sol
@@ -1,23 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// @notice The struct for a pointer to a message payload in a remote (or local) chain.
+/// @custom:field origin The origin address of the message.
+/// @custom:field blockNumber The block number of the message.
+/// @custom:field logIndex The log index of the message.
+/// @custom:field timestamp The timestamp of the message.
+/// @custom:field chainId The origin chain ID of the message.
 struct Identifier {
     address origin;
-    uint256 blockNumber;
-    uint256 logIndex;
-    uint256 timestamp;
+    uint64 blockNumber;
+    uint32 logIndex;
+    uint64 timestamp;
     uint256 chainId;
 }
 
-/// @title ICrossL2Inbox
-/// @notice Interface for the CrossL2Inbox contract.
 interface ICrossL2Inbox {
+    /// @notice Thrown when trying to execute a cross chain message on a deposit transaction.
+    error NoExecutingDeposits();
 
-    /// @notice Validates a cross chain message on the destination chain
-    ///         and emits an ExecutingMessage event. This function is useful
-    ///         for applications that understand the schema of the _message payload and want to
-    ///         process it in a custom way.
-    /// @param _id      Identifier of the message.
-    /// @param _msgHash Hash of the message payload to call target with.
+    /// @notice Thrown when trying to validate a cross chain message with an identifier checksum that is
+    ///         invalid or was not provided in the transaction's access list to set the slot as warm.
+    error NotInAccessList();
+
+    /// @notice Emitted when a message is being executed.
+    event ExecutingMessage(bytes32 indexed msgHash, Identifier id);
+
+    /// @notice Returns the semantic version of the contract.
+    /// @return version_ The semantic version.
+    function version() external view returns (string memory version_);
+
+    /// @notice Validates a message by checking that the identifier checksum slot is warm.
+    /// @dev    To process the message, the tx must include the checksum composed by the message's
+    ///         identifier and msgHash in the access list.
+    /// @param _id The identifier of the message.
+    /// @param _msgHash The hash of the message.
     function validateMessage(Identifier calldata _id, bytes32 _msgHash) external;
+
+    /// @notice Calculates the checksum of an identifier and message hash.
+    /// @param _id The identifier of the message.
+    /// @param _msgHash The hash of the message.
+    /// @return checksum_ The checksum of the identifier and message hash.
+    function calculateChecksum(Identifier memory _id, bytes32 _msgHash) external pure returns (bytes32 checksum_);
 }
