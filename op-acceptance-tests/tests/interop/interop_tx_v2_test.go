a5c7ebb8307c4459c0572f0dc4169a454bc5de0a
Park Changwan <changwan@oplabs.co>
2025-03-21 21:06:06 +0900

    clean test

---
+++ op-acceptance-tests/tests/interop/interop_tx_v2_test.go
@@ -0,0 +1,55 @@
+package interop
+
+import (
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/devnet-sdk/contracts/constants"
+	"github.com/ethereum-optimism/optimism/devnet-sdk/system"
+	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/systest"
+	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/testlib/validators"
+	"github.com/stretchr/testify/require"
+)
+
+func txInitAndExecMsg(
+	lowLevelSystemGetter validators.LowLevelSystemGetter,
+	l2ChainNums int,
+	chainIdxs []uint64,
+	walletGetters []validators.WalletGetter,
+) systest.InteropSystemTestFunc {
+	return func(t systest.T, sys system.InteropSystem) {
+		ctx, rng, logger, _, wallets, opts := DefaultSetup(t, lowLevelSystemGetter, l2ChainNums, chainIdxs, walletGetters)
+
+		eventLoggerAddress, err := DeployEventLogger(ctx, wallets[0], logger)
+		require.NoError(t, err)
+
+		// Intent to initiate message(or emit event) on chain A
+		txA := system.NewIntent[*system.InitTrigger, *system.InteropOutput](opts[0])
+		randomInitTrigger := RandomInitTrigger(rng, eventLoggerAddress, 3, 10)
+		txA.Content.Set(randomInitTrigger)
+
+		// Trigger single event
+		receiptA, err := txA.PlannedTx.Included.Eval(ctx)
+		require.NoError(t, err)
+		logger.Info("initiate message included", "block", receiptA.BlockHash)
+
+		// Intent to validate message(or emit event) on chain B
+		txB := system.NewIntent[*system.ExecTrigger, *system.InteropOutput](opts[1])
+		txB.Content.DependOn(&txA.Result)
+
+		// Single event in tx so index is 0
+		txB.Content.Fn(system.ExecuteIndexed(constants.CrossL2Inbox, &txA.Result, 0))
+
+		receiptB, err := txB.PlannedTx.Included.Eval(ctx)
+		require.NoError(t, err)
+		logger.Info("validate message included", "block", receiptB.BlockHash)
+	}
+}
+
+func TestTxInitAndExecMsg(t *testing.T) {
+	l2ChainNums := 2
+	chainIdxs, walletGetters, totalValidators, lowLevelSystemGetter := SetupDefaultInteropSystemTest(l2ChainNums)
+	systest.InteropSystemTest(t,
+		txInitAndExecMsg(lowLevelSystemGetter, l2ChainNums, chainIdxs, walletGetters),
+		totalValidators...,
+	)
+}
