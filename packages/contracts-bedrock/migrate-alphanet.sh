96380e9c730d575ccb5be29f1b43c2be51ae0178
Zach Howard <zach@oplabs.co>
2025-03-26 20:28:33 -0400

    adds addresses

--- packages/contracts-bedrock/migrate-alphanet.sh
+++ packages/contracts-bedrock/migrate-alphanet.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 just forge-build
-forge script -vvv scripts/deploy/InteropAlphanetMigration.s.sol:InteropAlphanetMigration $@
+forge script -vvvvv scripts/deploy/InteropAlphanetMigration.s.sol:InteropAlphanetMigration $@
--- packages/contracts-bedrock/scripts/deploy/InteropAlphanetMigration.s.sol
+++ packages/contracts-bedrock/scripts/deploy/InteropAlphanetMigration.s.sol
@@ -13,12 +13,12 @@ import { IOptimismPortal2 as IOptimismPortal } from "interfaces/L1/IOptimismPort
 
 contract InteropAlphanetMigration is Script {
     function run() public {
-        IOPContractsManager opcm = 0x0; // TODO
+        IOPContractsManager opcm = IOPContractsManager(0xEB32e20EbDE266A769a5683CC80976f05D9e6e7B);
         bytes32 absolutePrestate = hex"0387beeb10e2139751e069ad40f0d1f0fa91b6076fa6f2d5dd488d453a46eec6";
         bool usePermissionlessGame = true;
-        Proposal memory startingAnchorRoot = Proposal({ root: Hash.wrap(hex""), l2SequenceNumber: 0 }); // TODO
-        address proposer; // TODO
-        address challenger; // TODO
+        Proposal memory startingAnchorRoot = Proposal({ root: Hash.wrap(hex"dd69e5f8f65f27ed413cb31f80070ec961b3dd5ca8898269cade08699d9303f6"), l2SequenceNumber: 1743027458 });
+        address proposer = 0x4d522194aa103df731F2e6eB74cF2005FD6C48F5;
+        address challenger = 0x544078E6C0A7dFC220E096026E99ee87773d1624;
         uint64 maxGameDepth = 73;
         uint64 splitDepth = 30;
         uint256 initBond = 0.08 ether;
@@ -28,12 +28,12 @@ contract InteropAlphanetMigration is Script {
         IOPContractsManager.OpChainConfig[] memory opChainConfigs = new IOPContractsManager.OpChainConfig[](2);
         opChainConfigs[0] = IOPContractsManager.OpChainConfig({
             systemConfigProxy: ISystemConfig(0x38CFB302cdA19FD376bE2237D220D35C404A36bA),
-            proxyAdmin: IProxyAdmin(0x0),
+            proxyAdmin: IProxyAdmin(0x8a2dF05608B2AE0Eb75809b210527dd1d2705E31),
             absolutePrestate: Claim.wrap(absolutePrestate)
         });
-        opChainConfigs[0] = IOPContractsManager.OpChainConfig({
+        opChainConfigs[1] = IOPContractsManager.OpChainConfig({
             systemConfigProxy: ISystemConfig(0xCE1da8571d67d139A8040EBa35BEF8cfd34a0F2f),
-            proxyAdmin: IProxyAdmin(0x0),
+            proxyAdmin: IProxyAdmin(0x6220fd818ea70803B65DfABA7DEb8E72C8Fb396E),
             absolutePrestate: Claim.wrap(absolutePrestate)
         });
 
