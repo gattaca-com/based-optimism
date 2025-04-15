2d178797f90312307ad5efe9b88f9eadaef9979f
Adrian Sutton <adrian@oplabs.co>
2025-04-16 08:53:34 +1000

    cannon: Pick random version when fuzzing.

--- cannon/mipsevm/tests/fuzz_evm_multithreaded_test.go
+++ cannon/mipsevm/tests/fuzz_evm_multithreaded_test.go
@@ -17,8 +17,8 @@ import (
 func FuzzStateSyscallCloneMT(f *testing.F) {
 	versions := GetMultiThreadedTestCases(f)
 	require.NotZero(f, len(versions), "must have at least one multithreaded version supported")
-	f.Fuzz(func(t *testing.T, nextThreadId, stackPtr Word, seed int64) {
-		v := versions[len(versions)-1]
+	f.Fuzz(func(t *testing.T, nextThreadId, stackPtr Word, seed int64, version int) {
+		v := versions[version%len(versions)]
 		goVm := v.VMFactory(nil, os.Stdout, os.Stderr, testutil.CreateLogger(), testutil.WithRandomization(seed))
 		state := mttestutil.GetMtState(t, goVm)
 		// Update existing threads to avoid collision with nextThreadId
